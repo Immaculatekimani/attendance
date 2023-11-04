34128ecb806edef8f14c8576026e2fb9a03ca959
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-11-04 17:54:05 +0300

    Add register
    Added user registering functionality

---
+++ src/main/java/com/emma/action/UserAction.java
@@ -0,0 +1,29 @@
+package com.emma.action;
+
+import com.emma.app.model.entity.User;
+import com.emma.app.model.entity.UserRole;
+import com.emma.database.Database;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user")
+public class UserAction extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Database database = Database.getDbInstance();
+
+        String username = req.getParameter("new-username");
+        String password = req.getParameter("new-password");
+        String confirmPassword = req.getParameter("confirmNewPassword");
+
+        if (password.equals(confirmPassword))
+            database.getUsers().add(new User("E230", username, password, UserRole.ADMIN));
+
+        resp.sendRedirect("./");
+    }
+
+}
