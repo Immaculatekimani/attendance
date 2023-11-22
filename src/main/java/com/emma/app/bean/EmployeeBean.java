cd5d143f0686bf56f5240589d9fb800addf9936a
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-11-22 14:05:48 +0300

    EJB for employees
    Added EJB for employee

--- src/main/java/com/emma/app/bean/EmployeeBean.java
+++ src/main/java/com/emma/app/bean/EmployeeBean.java
@@ -3,9 +3,12 @@ package com.emma.app.bean;
 import com.emma.app.model.Employee;
 import com.emma.database.Database;
 
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
 import java.io.Serializable;
 import java.util.List;
-
+@Stateless(name = "attendance/EmployeeBean")
+@Remote
 public class EmployeeBean extends GenericBean<Employee> implements EmployeeBeanI {
 
 
